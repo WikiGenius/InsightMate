185067bdb46ba08b246280cdd89d1d4e9d9f3d07
WikiGenius <muhammedelyamani92@gmail.com>
2023-03-07 23:01:36 +0200

    Fix upload files

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.properties import ObjectProperty
 from kivy.uix.filechooser import FileChooserIconView
 from kivy.uix.screenmanager import Screen
 from plyer import filechooser
+from kivy.properties import ListProperty
 
 # Define the main widget for the app
 
@@ -13,40 +14,59 @@ Builder.load_file('InsightMate.kv')
 class MainScreen(Screen):
     pass       
 
-class FileUploader(FileChooserIconView):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.dirselect = False
-        self.filters = ['*.docx', '*.doc', '*.pdf']
         
 class InsightMateApp(MDApp):
-    
+    selection = ListProperty([])
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.doc_path = None
+        
     def build(self):
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.theme_style = "Dark"
-
-        # Create a new file chooser
-        self.file_chooser = filechooser.FileChooser()
-        self.file_chooser.filters = ['*.docx', '*.doc', '*.pdf']
-
-        self.file_path = ObjectProperty(None)
 
         self.screen = MainScreen(name='main')
 
         return self.screen
 
 
-def file_selected(self, selection):
-    self.file_path.text = selection[0]
-    self.file_chooser.path = os.path.dirname(selection[0])
+    def file_selected(self, selection):
+        self.file_path.text = selection[0]
+        self.file_chooser.path = os.path.dirname(selection[0])
 
     
     def summarize_document(self):
         # TODO: Add logic to summarize the entire document
+        print(self.doc_path )
         pass
     
     def summarize_pages(self):
         # TODO: Add logic to summarize each page of the document
+        print(self.doc_path )
+        
         pass
     
+    def choose(self):
+        '''
+        Call plyer filechooser API to run a filechooser Activity.
+        '''
+        filters = [ '*.pdf', '*.docx', '*.doc']  # add more video file extensions here
+        path = './docs/*'
+        filechooser.open_file(filters=filters, on_selection=self.handle_selection, path=path)
+
+    def handle_selection(self, selection):
+        '''
+        Callback function for handling the selection response from Activity.
+        '''
+        if selection is not None:
+            self.selection = selection
+
+    def on_selection(self, *a, **k):
+        '''
+        Update TextInput.text after FileChoose.selection is changed
+        via FileChoose.handle_selection.
+        '''
+        self.doc_path = self.selection[0]
+
+        
 InsightMateApp().run()
