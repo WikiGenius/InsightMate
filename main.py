c8e69e32a4453d492221e663d52abbf43befa33a
WikiGenius <muhammedelyamani92@gmail.com>
2023-03-07 23:31:00 +0200

    add sprogress

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from utils.layout import *  
 from utils import get_summaries, get_summary_doc, generate_report_summaries, generate_report_summary_doc, get_title_paper
         
@@ -9,6 +8,9 @@ class InsightMateApp(MDApp):
         super().__init__(**kwargs)
         self.doc_path = None
         self.summaries = None
+        self.progress_percent = 0  # initialize progress percentage to 0
+
+        
     def build(self):
         self.theme_cls.primary_palette = "Blue"
         self.theme_cls.theme_style = "Dark"
@@ -19,16 +21,20 @@ class InsightMateApp(MDApp):
 
     
     def summarize_document(self):
-        # TODO: Add logic to summarize the entire document
         if self.summaries is not None:
             self.summary_doc = get_summary_doc(self.summaries)
             generate_report_summary_doc(self.title_paper, self.summary_doc)
+            self.progress_percent = 100  # set progress percentage to 100 when done
+        self.screen.progress.value = self.progress_percent  # update progress bar
+
     def summarize_pages(self):
-        # TODO: Add logic to summarize each page of the document
+        print(self.selection)
         if self.doc_path is not None:
             self.summaries = get_summaries(doc_path = self.doc_path )
             generate_report_summaries(self.title_paper, self.summaries)
-    
+            self.progress_percent = 100  # set progress percentage to 100 when done
+        self.screen.progress.value = self.progress_percent  # update progress bar
+        
     def choose(self):
         '''
         Call plyer filechooser API to run a filechooser Activity.
